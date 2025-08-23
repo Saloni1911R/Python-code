0539a993d1efec501a33628d327ada513a6c984b
Saloni Ramavat <183176394+Saloni1911R@users.noreply.github.com>
2025-08-23 09:32:08 +0530

    Add files via upload

---
+++ Lab_7/once_zeros_arr.py
@@ -0,0 +1,7 @@
+#Write a NumPy program to create an array of ones and zeros.
+import numpy as np
+
+zeros_arr = np.zeros((3, 3))  # 3x3 array of zeros
+ones_arr = np.ones((3, 3))    # 3x3 array of ones
+print("Array of zeros:\n", zeros_arr)
+print("Array of ones:\n", ones_arr)
