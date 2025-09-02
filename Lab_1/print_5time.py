d9f37c35d56551b5e5aec5d950d07d0a6c7c1445
Saloni Ramavat <183176394+Saloni1911R@users.noreply.github.com>
2025-09-02 11:11:12 +0530

    Add files via upload

---
+++ Lab_1/print_5time.py
@@ -0,0 +1,3 @@
+#a.	Write a program that displays “Welcome to Python” five times.
+for i in range(5):
+    print("Welcome to Python")
---
+++ Lab_1/table.py
@@ -0,0 +1,4 @@
+#tabel
+print("Number   Square   Cube")
+for i in range(1, 6):
+    print(f"{i:<8}{i**2:<8}{i**3}")
